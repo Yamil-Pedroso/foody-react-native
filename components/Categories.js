da93d1905895466960fb41efc70d2bab4a700fe0
Yami Pedroso-AI <55386637+Yamil-Pedroso@users.noreply.github.com>
2022-11-06 12:44:22 +0100

    In Categories.js changed paddingTop

--- components/Categories.js
+++ components/Categories.js
@@ -17,7 +17,7 @@ const Categories = () => {
       <ScrollView
         contentContainerStyle={{
             paddingHorizontal: 15,
-            paddingTop: 8,
+            paddingTop: 6,
         }}
         horizontal
         showsHorizontalScrollIndicator={false}
